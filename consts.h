14593094db7f0a483b5fd57ab3be0a57b160b1f9
daherb <sonofchaos@gmx.de>
2013-04-22 18:47:06 +0200

    Exported common constants

---
+++ consts.h
@@ -0,0 +1,16 @@
+// Syllable ist file
+//#define SYLLABLE_LIST "../Silben-data/silbenliste.txt"
+#define SYLLABLE_LIST "/home/herb/src/Silben-data/silbenliste.txt"
+
+#define WORD_FREQ_LIST "/home/herb/src/Silben-data/frequenzliste.txt.cleaner"
+
+// PVM messages
+#define DIE 1
+#define DATA 2
+#define NEXT 3
+
+// Number of words in one block
+#define BLOCK_SIZE 10
+
+// Number of threads spawned on a host
+#define PER_HOST_TASK_COUNT 1
