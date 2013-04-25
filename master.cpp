4c37f4e6917e64000c3b7d52f55687cef1da2ed9
daherb <sonofchaos@gmx.de>
2013-04-25 10:55:30 +0200

    At the moment working master but forgetting the data for output

--- master.cpp
+++ master.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <pvm3.h>
-#include <mcheck.h>
+#include <ctime>
 #include "consts.h"
 
 using namespace std;
@@ -35,13 +35,16 @@ int main(int argc, char ** argv)
   int task_count=host_count*PER_HOST_TASK_COUNT;
   // Task-IDs
   int tids[task_count];
-  int r=pvm_spawn((char *)"silben_slave",NULL,PvmTaskDefault,NULL,task_count,tids);
+  char **args = NULL;
+  int r=pvm_spawn((char *)"silben_slave",args,PvmTaskDefault /*PvmTaskDebug*/,NULL,task_count,tids);
   if (r<task_count)
   {
     for(int ct=0;ct<task_count;ct++)
       printf("Spawn error %d\n",tids[ct]);
   }
   log << "finished spawning" << endl;
+  time_t begin_time, end_time;
+  time(&begin_time);
   unordered_map<string,int>::iterator it = frequency_list.begin();
   char *word = (char *) malloc(48);
   char *tree = (char *) malloc(1024*1024);
@@ -49,7 +52,7 @@ int main(int argc, char ** argv)
   while (it != frequency_list.end())
     {
       // See if DATA is pending
-      if (pvm_probe(-1,DATA))
+      while (pvm_probe(-1,DATA))
 	{
 	  int recbuf=pvm_recv(-1,DATA);
 	  log << "got DATA " << endl;
@@ -82,13 +85,87 @@ int main(int argc, char ** argv)
 	{
 	  log << "Pack " << it->first << endl;
 	  pvm_pkstr((char *)it->first.c_str());
+	  unordered_map<string,int>::iterator prev=it;
 	  it++;
+	  frequency_list.erase(prev);
 	}
       int sbuf = pvm_send(tid,DATA);
       log << "send DATA with " << sbuf << endl;
     }
-  log << "Exiting" << endl;
+  // See if DATA is pending
+  while (pvm_probe(-1,DATA))
+    {
+      int recbuf=pvm_recv(-1,DATA);
+      log << "got DATA " << endl;
+      int block_size;
+      pvm_upkint(&block_size,1,1);
+      log << "Recieving data block of size " << block_size << endl;
+      for (int ct = 0; ct < block_size; ct++)
+	{
+	  pvm_upkstr(word);
+	  pvm_upkstr(tree);
+	  string sword = string(word);
+	  string stree = string(tree);
+	  log << "Unpacked " << sword << " \t" << sword.size() << " and " << stree << " \t" << stree.size() << endl;
+	  tree_list[sword] = stree;
+	}
+    }
+  // 
+  // for(int ct = 0; ct < task_count; ct++)
+  //   {
+  //     pvm_initsend(PvmDataDefault);
+  //     int null = 0;
+  //     pvm_pkint(&null,1,1);
+  //     pvm_send(tids[ct],DATA);
+  //     log << "Send empty DATA to " << tids[ct] << endl;
+  //   }
+
+  // Syncing up after end of data
+  for (int ct = 0; ct < task_count; ct++)
+    {
+      pvm_initsend(PvmDataDefault);
+      int null = 0;
+      pvm_pkint(&null,1,1);
+      pvm_send(tids[ct],DATA);
+      pvm_initsend(PvmDataDefault);
+      pvm_send(tids[ct],DATA);
+      log << "Send empty DATA to " << tids[ct] << endl;
+      int recbuf=pvm_recv(tids[ct],FINAL);
+      log << "got FINAL " << endl;
+      int block_size;
+      pvm_upkint(&block_size,1,1);
+      log << "Recieving data block of size " << block_size << endl;
+      for (int ct2 = 0; ct2 < block_size; ct2++)
+  	{
+  	  pvm_upkstr(word);
+  	  pvm_upkstr(tree);
+  	  string sword = string(word);
+  	  string stree = string(tree);
+  	  log << "Unpacked " << sword << " \t" << sword.size() << " and " << stree << " \t" << stree.size() << endl;
+  	  tree_list[sword] = stree;
+  	}
+      int tid;
+      pvm_bufinfo(recbuf,NULL,NULL,&tid);
+      pvm_initsend(PvmDataDefault);
+      int sbuf = pvm_send(tids[ct],DIE);
+      log << "send DIE with " << sbuf << endl;
+    }
+  time(&end_time);
+  log << "Matching took " << difftime(end_time,begin_time) << " seconds" << endl;
+  log << "Start writing output" << endl;
+  ofstream ofile;
+  int wordcount = 0;
+  ofile.open(OUTPUT_FILE);
+  for (unordered_map<string,int>::iterator it = frequency_list.begin(); it != frequency_list.end(); it++)
+    {
+      log << it->first << "\t" << tree_list[it->first] << "\t" << it->second << endl;
+      ofile << it->first << "\t" << tree_list[it->first] << "\t" << it->second << endl;
+    }
+  ofile.close();
+  log << "Finished writing output" << endl;
+  log.close();
   free(word);
   free(tree);
-  log.close();
+  log << "Exiting" << endl;
+  pvm_exit();
 }
