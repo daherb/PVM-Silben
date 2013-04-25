4671e306636895931a2d8a2b76484fd2c352744b
daherb <sonofchaos@gmx.de>
2013-04-25 12:37:52 +0200

    Changed some constants

--- consts.h
+++ consts.h
@@ -1,20 +1,21 @@
 // Syllable list file
 //#define SYLLABLE_LIST "../Silben-data/silbenliste.txt"
-#define SYLLABLE_LIST "/home/herb/src/Silben-data/silbenliste.txt"
+#define SYLLABLE_LIST "~/src/Silben-data/silbenliste.txt"
 
 // Word list file
-#define WORD_FREQ_LIST "/home/herb/src/Silben-data/frequenzliste.txt"
+#define WORD_FREQ_LIST "~/src/Silben-data/frequenzliste.txt"
 
 // Output file
-#define OUTPUT_FILE "partition.txt"
+#define OUTPUT_FILE "/tmp/partition.txt"
 
 // PVM messages
 #define DIE 1
 #define DATA 2
-#define NEXT 3
+#define NEXT 4
+#define FINAL 8
 
 // Number of words in one block
-#define BLOCK_SIZE 100
+#define BLOCK_SIZE 1000
 
 // Number of threads spawned on a host
-#define PER_HOST_TASK_COUNT 1
+#define PER_HOST_TASK_COUNT 3
